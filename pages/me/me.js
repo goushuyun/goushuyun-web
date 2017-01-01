590e29b648778e335f0a89514f411fe158fdca6a
Wang-Kai <persistence2013@sina.com>
2017-01-01 10:26:46 +0800

    完成 me 页面基础

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,40 +1,16 @@
-var types = ['default', 'primary', 'warn']
-var pageObject = {
-  data: {
-    defaultSize: 'default',
-    primarySize: 'default',
-    warnSize: 'default',
-    disabled: false,
-    plain: false,
-    loading: false
-  },
-  setDisabled: function(e) {
-    this.setData({
-      disabled: !this.data.disabled
-    })
-  },
-  setPlain: function(e) {
-    this.setData({
-      plain: !this.data.plain
-    })
-  },
-  setLoading: function(e) {
-    this.setData({
-      loading: !this.data.loading
-    })
-  }
-}
-
-for (var i = 0; i < types.length; ++i) {
-  (function(type) {
-    pageObject[type] = function(e) {
-      var key = type + 'Size'
-      var changedData = {}
-      changedData[key] =
-        this.data[key] === 'default' ? 'mini' : 'default'
-      this.setData(changedData)
+Page({
+    data: {
+        tabs: ['全部','待付款','待发货','待收货','待评价'],
+        currentPage: 0
+    },
+    goToPage(e){
+        this.setData({
+            currentPage: e.target.dataset.index
+        })
+    },
+    pageChange(e){
+        this.setData({
+            currentPage: e.detail.current
+        })
     }
-  })(types[i])
-}
-
-Page(pageObject)
+})
