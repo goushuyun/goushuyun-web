f50b2b203d15a1b8307bdd1844438e4264c57760
Wang-Kai <persistence2013@sina.com>
2017-01-19 14:57:44 +0800

    为 me 页面中的订单加了按钮

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,16 +1,37 @@
 Page({
     data: {
-        tabs: ['全部','待付款','待发货','待收货','待评价'],
+        tabs: ['全部', '待付款', '待发货', '待收货', '待评价'],
         currentPage: 0
     },
-    goToPage(e){
+    goToPage(e) {
         this.setData({
             currentPage: e.target.dataset.index
         })
     },
-    pageChange(e){
+    call() {
+        console.log('asdfasdfasd')
+        wx.makePhoneCall({
+            phoneNumber: '18817953402'
+        })
+    },
+    pageChange(e) {
         this.setData({
             currentPage: e.detail.current
         })
+    },
+    cancel_order() {
+        wx.showModal({
+            // title: '提示',
+            content: '您确定要取消该订单吗？',
+            success: function(res) {
+                if (res.confirm) {
+                    console.log('用户点击确定')
+                }
+            }
+        })
     }
+
+
+
+
 })
