73b1bb9e8906d4340cf9c20b5e0a9a0e8be65550
Wang-Kai <persistence2013@sina.com>
2017-02-04 16:27:53 +0800

    增加搜索功能

--- pages/booksList/booksList.js
+++ pages/booksList/booksList.js
@@ -4,22 +4,121 @@ var WxSearch = require('../../wxSearch/wxSearch.js')
 var app = getApp()
 Page({
     data: {
-        // wxSearchData:{
-        //   view:{
-        //     isShow: true
-        //   }
-        // }
+        //books to list
+        books: [],
+
+        page: 1,
+        size: 10
     },
-    onLoad: function() {
-        console.log('onLoad')
-        var that = this
+
+    onLoad: function(options) {
+        console.log(options)
+        var self = this
+
+        if(options.search_val != undefined){
+            let search_val = options.search_val
+            //search_val is not null
+            var app = getApp(), data = {}
+            data.shop_id = app.shop_id
+            data.page = this.data.page
+            data.size = this.data.size
+            //according search_val, to searching
+            if(/^\d{10,13}$/.test(search_val)){
+                //the value is isbn
+                data.isbn = search_val
+            }else{
+                //the value is text, maybe title, author, publisher
+                data.author = data.publisher = data.title = search_val
+            }
+
+            console.log("********** The request data *********")
+            console.log(data)
+            wx.request({
+                url: 'https://app.cumpusbox.com/v1/books/listBooks',
+                method: "POST",
+                data: data,
+                success(res){
+                    //set new result books
+                    self.setData({
+                        books: res.data.data
+                    })
+
+                    console.log(res)
+                }
+            })
+
+            return false
+        }
+
+
+        //list all books of this category
+        var app = getApp(), data = {}
+        data.shop_id = app.shop_id
+        data.category = options.category
+        data.page = this.data.page
+        data.size = this.data.size
+        console.log(data)
+
+
+
+        wx.request({
+            url: 'https://app.cumpusbox.com/v1/books/listBooks',
+            method: 'POST',
+            data: data,
+            success(res){
+                console.log(res)
+
+                if(res.data.code == '00000'){
+                    console.log(res.data.data)
+
+
+                    // query data success
+                    self.setData({
+                        books: self.data.books.concat(res.data.data)
+                    })
+
+                    console.log(self.data)
+                }
+
+            }
+        })
+
         //初始化的时候渲染wxSearchdata
-        WxSearch.init(that, 44, ['weappdev', '小程序', 'wxParse', 'wxSearch', 'wxNotification']);
+        WxSearch.init(self, 44, ['weappdev', '小程序', 'wxParse', 'wxSearch', 'wxNotification']);
         WxSearch.initMindKeys(['weappdev.com', '微信小程序开发', '微信开发', '微信小程序']);
     },
+
     wxSearchFn: function(e) {
         var that = this
         WxSearch.wxSearchAddHisKey(that);
+
+        //the value in input is this.data.wxSearchData.value
+        let search_val = this.data.wxSearchData.value, data = {page: this.data.page, size: this.data.size}, app = getApp()
+
+        data.shop_id = app.shop_id
+        if(/^\d{10,13}$/.test(search_val)){
+            // isbn
+            data.isbn = search_val
+        }else{
+            //text
+            data.title = data.author = data.publisher = search_val
+        }
+
+        wx.request({
+            url: 'https://app.cumpusbox.com/v1/books/listBooks',
+            method: "POST",
+            data: data,
+            success(res){
+                //set new result books
+                that.setData({
+                    books: res.data.data
+                })
+
+                console.log(res)
+            }
+        })
+
+
 
     },
     wxSearchInput: function(e) {
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,21 @@
 Page({
+    data: {
+        search_val: ''
+    },
+    bindchange(e){
+        this.setData({
+            search_val: e.detail.value
+        })
+    },
+    search(e){
+        wx.navigateTo({
+            url: '/pages/booksList/booksList?search_val=' + this.data.search_val
+        })
+    },
 
+    chooseCategory(e){
+        wx.navigateTo({
+            url: '/pages/booksList/booksList?category=' + e.currentTarget.dataset.category
+        })
+    }
 })
