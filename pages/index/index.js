31a7e3db0a5239228aabfacfbce2f1c43391cb90
Wang-Kai <persistence2013@sina.com>
2017-02-11 21:46:50 +0800

    完成自动获取学校功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,68 @@
+var QQMapWX = require('../../libs/qqmap-wx-jssdk.min.js')
+var qqmapsdk
+
 Page({
+    onLoad: function() {
+        // 实例化API核心类
+        qqmapsdk = new QQMapWX({
+            key: 'Q4XBZ-OV6W4-636UU-D7BLU-RI7SH-65FWN'
+        })
+    },
+    chooseAddress(){
+
+        console.log('kaiakiakaiu')
+
+        var self = this, school
+
+        wx.chooseLocation({
+            success(res){
+                if(res.name != ''){
+                    school = res.name
+                }else{
+                    school = res.address
+                }
+
+                if(school.length > 10){
+                    school = school.substr(0,10) + '...'
+                }
+
+                self.setData({
+                    'school': school
+                })
+            }
+        })
+    },
+    onReady() {
+        // 调用接口
+        let self = this, school
+        qqmapsdk.search({
+            keyword: '大学',
+            success: function(res) {
+
+                if(res.data.length > 0){
+                    school = res.data[0].title
+                }else{
+                    school = '请选择学校'
+                }
+
+                self.setData({
+                    school: school
+                })
+            },
+            fail: function(res) {
+                console.log('---------------------------')
+                console.log(res)
+                console.log('---------------------------')
+            }
+        })
+    },
+
+
     data: {
-        search_val: ''
+        search_val: '',
+        school: ''
     },
     scan() {
-        console.log('scan')
-
         wx.scanCode({
             success: (res) => {
                 let isbn = res.result
