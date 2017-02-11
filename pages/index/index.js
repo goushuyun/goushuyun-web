0a71cc42de2e9e9a320037ad793b013fd984a31e
Wang-Kai <persistence2013@sina.com>
2017-02-11 20:22:15 +0800

    完成扫码搜索

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,18 +2,30 @@ Page({
     data: {
         search_val: ''
     },
-    bindchange(e){
+    scan() {
+        console.log('scan')
+
+        wx.scanCode({
+            success: (res) => {
+                let isbn = res.result
+                wx.navigateTo({
+                    url: '/pages/booksList/booksList?search_val=' + isbn
+                })
+            }
+        })
+    },
+    bindchange(e) {
         this.setData({
             search_val: e.detail.value
         })
     },
-    search(e){
+    search(e) {
         wx.navigateTo({
             url: '/pages/booksList/booksList?search_val=' + this.data.search_val
         })
     },
 
-    chooseCategory(e){
+    chooseCategory(e) {
         wx.navigateTo({
             url: '/pages/booksList/booksList?category=' + e.currentTarget.dataset.category
         })
