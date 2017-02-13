dbab8812c079069d1d85f157ae8be1ba01c4a2b2
Wang-Kai <persistence2013@sina.com>
2017-02-13 14:00:00 +0800

    save school into storage

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,45 +8,57 @@ Page({
             key: 'Q4XBZ-OV6W4-636UU-D7BLU-RI7SH-65FWN'
         })
     },
-    chooseAddress(){
+    chooseAddress() {
 
         console.log('kaiakiakaiu')
 
-        var self = this, school
+        var self = this,
+            school
 
         wx.chooseLocation({
-            success(res){
-                if(res.name != ''){
+            success(res) {
+                if (res.name != '') {
                     school = res.name
-                }else{
+                } else {
                     school = res.address
                 }
 
-                if(school.length > 10){
-                    school = school.substr(0,10) + '...'
+                if (school.length > 10) {
+                    school = school.substr(0, 10) + '...'
                 }
 
                 self.setData({
                     'school': school
                 })
+
+                wx.setStorage({
+                    key: "school",
+                    data: school
+                })
+
             }
         })
     },
     onReady() {
         // 调用接口
-        let self = this, school
+        let self = this,
+            school
         qqmapsdk.search({
             keyword: '大学',
             success: function(res) {
 
-                if(res.data.length > 0){
+                if (res.data.length > 0) {
                     school = res.data[0].title
-                }else{
+                } else {
                     school = '请选择学校'
                 }
 
                 self.setData({
                     school: school
+                })
+                wx.setStorage({
+                    key: "school",
+                    data: school
                 })
             },
             fail: function(res) {
