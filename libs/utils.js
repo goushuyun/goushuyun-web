35b325918be00767343401c9cf7fb6e462382df8
Wang-Kai <persistence2013@sina.com>
2017-02-16 10:32:02 +0800

    基本完成me页面

--- libs/utils.js
+++ libs/utils.js
@@ -1,18 +1,8 @@
 function unixTimestamp2DateStr(unixTimestamp) {
 
-    console.log('---------------------------')
-
-    console.log(unixTimestamp)
-
-    console.log('---------------------------')
-
     var date = new Date(unixTimestamp * 1000);
 
-    console.log(date)
-
-    // Hours part from the timestamp
     var hours = date.getHours();
-    // Minutes part from the timestamp
     var minutes = "0" + date.getMinutes();
 
     var year = date.getFullYear()
@@ -20,11 +10,7 @@ function unixTimestamp2DateStr(unixTimestamp) {
     var month = ('0' + (date.getMonth() +1 )).substr(-2)
     var day = date.getDate()
 
-    // Will display time in 10:30:23 format
     var formattedTime = year + '-' + month +'-' + day + ' ' + hours + ':' + minutes.substr(-2)
-
-
-    console.log(formattedTime)
 
     return formattedTime
 }
--- pages/me/me.js
+++ pages/me/me.js
@@ -11,8 +11,12 @@ Page({
         //订单信息
         user_id: '',
         orders: [],
+        wait_pay_orders: [],
+        wait_send_orders: [],
+        wait_accept_orders: [],
+        accepted_orders: [],
         page: 1,
-        size: 10,
+        size: 100,
         total: 0,
         order_status: 0
     },
@@ -25,11 +29,8 @@ Page({
             page: this.data.page,
             size: this.data.size,
             order_status: this.data.currentPage,
-            user_id: this.data.user_id,
-
+            user_id: this.data.user_id
         }
-
-        console.log(data)
 
         var self = this
         wx.request({
@@ -40,26 +41,61 @@ Page({
 
                 var respData = res.data
 
-                console.log(respData)
-
                 if(respData.code == '00000'){
+
+                    var orders = respData.data.map(el=>{
+                        el.total_price = (el.total_price/100).toFixed(2)
+                        el.freight = (el.freight/100).toFixed(2)
+
+                        for (var i = 0; i < el.items.length; i++) {
+                            el.items[i].book_price = (el.items[i].book_price/100).toFixed(2)
+                        }
+
+                        el.order_at_time = utils.unixTimestamp2DateStr(el.order_at)
+
+                        return el
+                    })
+
+                    var wait_pay_orders = []
+                    var wait_send_orders = []
+                    var wait_accept_orders = []
+                    var accepted_orders = []
+
+
+                    //每次拿到 orders 数据之后，根据订单状态将订单拆分
+                    for (var i = 0; i < orders.length; i++) {
+                        switch (orders[i].order_status) {
+                            case 1:
+                                wait_pay_orders.push(orders[i])
+                                break;
+                            case 2:
+                                wait_send_orders.push(orders[i])
+                                break;
+                            case 3:
+                                wait_accept_orders.push(orders[i])
+                                break;
+                            case 4:
+                                accepted_orders.push(orders[i])
+                                break;
+                        }
+                    }
+
+                    console.log(wait_send_orders)
+
+
                     self.setData({
                         total: respData.total,
-                        orders: respData.data.map(el=>{
-                            el.total_price = (el.total_price/100).toFixed(2)
-                            el.freight = (el.freight/100).toFixed(2)
-
-                            for (var i = 0; i < el.items.length; i++) {
-                                el.items[i].book_price = (el.items[i].book_price/100).toFixed(2)
-                            }
-
-                            el.order_at_time = utils.unixTimestamp2DateStr(el.order_at)
-
-                            console.log(el.order_at_time)
-
-                            return el
-                        })
+                        orders: orders,
                     })
+                    // wait_pay_orders: wait_pay_orders,
+                    // wait_send_orders: wait_pay_orders,
+                    // wait_accept_orders: wait_accept_orders,
+                    // accepted_orders: accepted_orders
+
+                    self.setData({
+                        wait_send_orders
+                    })
+
 
                 }
 
@@ -95,8 +131,6 @@ Page({
         this.setData({
             currentPage: e.target.dataset.index
         })
-
-        this.getOrders()
     },
     callSeller() {
 
